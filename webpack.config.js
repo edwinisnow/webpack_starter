36bee44ec6814092d9424e07f2bf01412511c259
EDWIN GEORGE LAZER <edwin04@gmail.com>
2023-09-21 05:27:16 +0100

    plugins, htmlwebplugin, template

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     mode: 'development',
     entry: {
@@ -15,5 +17,12 @@ module.exports = {
                 use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html'
+        })
+    ]
 }
